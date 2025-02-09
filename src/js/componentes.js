d1f52cc562ce8760629b8b8f367b9a79bf3f783c
Cypherx14 <alexcoro14xd@gmail.com>
2025-02-09 16:02:46 -0500

    Agregando evento para crear Todo

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,12 +1,17 @@
+//Importaciones
+import { Todo } from "../classes";
+import { todoList } from "../index";
+
 //referencias del html
 const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
 
 
 export const crearTodoHtml = (todo) => {
     
-    const htmlTodo = `<li class="completed" data-id="abc">
+    const htmlTodo = `<li class="${(todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
 						<div class="view">
-							<input class="toggle" type="checkbox" checked>
+							<input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : ''}>
 							<label>${todo.tarea}</label>
 							<button class="destroy"></button>
 						</div>
@@ -15,7 +20,22 @@ export const crearTodoHtml = (todo) => {
     const div = document.createElement('div'); //div para insertar todo el contenido del li al mismo
     div.innerHTML = htmlTodo;
     
-    divTodoList.append(div);
+    divTodoList.append(div.firstElementChild);
 
-    return div;
+    return div.firstElementChild;
 }
+
+//Eventos
+txtInput.addEventListener('keyup', (evento)=>{
+	if(evento.keyCode === 13 && txtInput.value.length > 0){
+		const nuevoTodo = new Todo(txtInput.value);
+		//insertar en el arreglo de todolist
+		todoList.nuevoTodo(nuevoTodo);
+		//insertar en el HTML
+		crearTodoHtml(nuevoTodo);
+		console.log(todoList);
+		
+		//eliminar el texto
+		txtInput.value = '';
+	}
+});
