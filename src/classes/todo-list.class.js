63a413c88c4d0e41e1a48de617da5284b3ea29aa
Cypherx14 <alexcoro14xd@gmail.com>
2025-02-09 16:48:40 -0500

    Agregando metodos de marcar completados y eliminar completados

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -7,15 +7,20 @@ export class TodoList{
     }
 
     eliminarTodo(id){
-        
+        this.todos = this.todos.filter( todo => todo.id != id);
     }
 
     marcarCompletado(id){
-
+        for(const todo of this.todos){
+            if(todo.id == id){ //recibo un numero pero JS lo transforma a String para poder comparar
+                todo.completado = !todo.completado;
+                break;
+            }
+        }
     }
 
     eliminarCompletados(){
-
+        this.todos = this.todos.filter( todo => !todo.completado);
     }
 
 }
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -5,10 +5,9 @@ import { todoList } from "../index";
 //referencias del html
 const divTodoList = document.querySelector('.todo-list');
 const txtInput = document.querySelector('.new-todo');
-
-
+const btnBorrar = document.querySelector('.clear-completed');
+ 
 export const crearTodoHtml = (todo) => {
-    
     const htmlTodo = `<li class="${(todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
 						<div class="view">
 							<input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : ''}>
@@ -19,9 +18,7 @@ export const crearTodoHtml = (todo) => {
 					</li>`;
     const div = document.createElement('div'); //div para insertar todo el contenido del li al mismo
     div.innerHTML = htmlTodo;
-    
     divTodoList.append(div.firstElementChild);
-
     return div.firstElementChild;
 }
 
@@ -34,8 +31,35 @@ txtInput.addEventListener('keyup', (evento)=>{
 		//insertar en el HTML
 		crearTodoHtml(nuevoTodo);
 		console.log(todoList);
-		
 		//eliminar el texto
 		txtInput.value = '';
+	}
+});
+
+divTodoList.addEventListener('click', (event) => {
+	
+	const nombreElemento = event.target.localName;  //input, label o boton
+	const todoElemento = event.target.parentElement.parentElement; //para obtener todo el li
+	const todoId = todoElemento.getAttribute('data-id');
+	
+	if(nombreElemento.includes('input')){// hizo click en el checkbox
+		todoList.marcarCompletado(todoId);
+		// agregar la clase para tachar 
+		todoElemento.classList.toggle('completed');
+	}else if(nombreElemento.includes('button')){
+		todoList.eliminarTodo(todoId);
+		//eliminar en el HTML
+		divTodoList.removeChild(todoElemento);
+	}	
+});
+
+btnBorrar.addEventListener('click', ()=>{
+	`todoList.eliminarCompletados();`  //elimina del arreglo de Todos
+	//eliminar del HTML
+	for (let i = divTodoList.children.length-1; i >=0; i--) {
+		const elemento = divTodoList.children[i];
+		if(elemento.classList.contains('completed')){
+			divTodoList.removeChild(elemento);
+		}	
 	}
 });
